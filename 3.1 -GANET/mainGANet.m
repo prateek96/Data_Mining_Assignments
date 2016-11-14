06d5b5512c3aabb2cfc45369bae32f7960e9639a
prateek96 <prateekthakur1@gmail.com>
2016-11-15 03:13:13 +0530

    Add files via upload
    Code for Ganet

---
+++ 3.1 -GANET/mainGANet.m
@@ -0,0 +1,56 @@
+function CC = mainGANet(fileinput,alfa,fileclass,numclass)
+
+if nargin >=2
+    
+edges=load(fileinput);
+
+mutationRate=0.2;
+CrossoverFraction=0.8;
+maxGen=50;  
+PopSize=50; 
+
+CC=ganet(edges,alfa,mutationRate,CrossoverFraction,maxGen,PopSize)
+
+[fid, message] = fopen('ris1.txt','wt');
+
+for i1 = 1:size(CC,2)
+    listnodes=CC{i1};
+    for j1= 1:size(listnodes,2)
+       
+            fprintf(fid,'%d ',listnodes(j1));
+    end
+    fprintf(fid,'\n');
+end
+      if nargin ==4
+          
+      class=load(fileclass);
+      numnodes=size(class,1)
+      CM=confusionMatrix(CC,class,numclass) 
+      NMI = computeNMI(CM,numnodes)
+      end
+else
+    error('not enough input parameters')
+end
+
+end
+
+
+
+
+function CM= confusionMatrix(CC,classi,numclass)
+
+CM = zeros(numclass,size(CC,2));
+
+for k=1:size(CC,2) 
+   
+    listnodes=CC{k};
+    for j = 1: size(listnodes,2)
+    nodo = listnodes(j);
+          if (nodo~=0)
+              classe = classi(nodo,2);
+              
+              CM(classe,k)=  CM(classe,k)+1;
+          end
+    end
+end
+end
